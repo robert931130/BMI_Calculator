032a08a72df9bee6007003d3b3e9d11f5b0a90a3
Robert <robert@gmail.com>
2026-03-29 01:39:39 +0800

    加入專案檔案。

---
+++ BMI_Calculator/BMI 計算機.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace BMI_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pbBodyImage = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBodyImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(21, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "身高(cm)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(21, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "體重(kg)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtHeight.Location = new System.Drawing.Point(26, 144);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(297, 29);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            this.txtHeight.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtHeight_KeyDown_1);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtWeight.Location = new System.Drawing.Point(26, 246);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(297, 29);
+            this.txtWeight.TabIndex = 3;
+            this.txtWeight.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtWeight_KeyDown_1);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCalculate.Location = new System.Drawing.Point(26, 303);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(297, 67);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "計算 BMI";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.lblResult.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblResult.Location = new System.Drawing.Point(35, 293);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 31);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(15, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 61);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "BMI 計算機";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 450);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.lblPrompt);
+            this.panel2.Controls.Add(this.lblStatus);
+            this.panel2.Controls.Add(this.pbBodyImage);
+            this.panel2.Controls.Add(this.lblResult);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(352, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(335, 450);
+            this.panel2.TabIndex = 10;
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblPrompt.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblPrompt.Location = new System.Drawing.Point(36, 203);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(264, 26);
+            this.lblPrompt.TabIndex = 8;
+            this.lblPrompt.Text = "請於左側輸入數值開始計算";
+            this.lblPrompt.Click += new System.EventHandler(this.lblPrompt_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblStatus.Location = new System.Drawing.Point(35, 339);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 31);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // pbBodyImage
+            // 
+            this.pbBodyImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbBodyImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbBodyImage.Location = new System.Drawing.Point(67, 77);
+            this.pbBodyImage.Name = "pbBodyImage";
+            this.pbBodyImage.Size = new System.Drawing.Size(203, 218);
+            this.pbBodyImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBodyImage.TabIndex = 7;
+            this.pbBodyImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "BMI 計算機";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBodyImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pbBodyImage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblPrompt;
+    }
+}
+
---
+++ BMI_Calculator/BMI 計算機.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHeight_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtWeight.Focus(); // 焦點移至體重
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        // 體重欄位：按 Enter 直接計算
+        private void txtWeight_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnCalculate.PerformClick(); // 模擬點擊計算按鈕
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // --- 加入輸入驗證功能 ---
+            bool isHeightValid = double.TryParse(txtHeight.Text, out double height);
+            bool isWeightValid = double.TryParse(txtWeight.Text, out double weight);
+
+            // 驗證身高輸入
+            if (isHeightValid)
+            {
+                if (height <= 0)
+                {
+                    MessageBox.Show("身高必須大於零。", "身高值錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的身高數值。", "身高值錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 驗證體重輸入
+            if (isWeightValid)
+            {
+                if (weight <= 0)
+                {
+                    MessageBox.Show("體重必須大於零。", "體重值錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的體重數值。", "體重值錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double h_m = height / 100;
+            double bmi = Math.Round(weight / (h_m * h_m), 1);
+
+            // 切換顯示狀態
+            lblPrompt.Visible = false;
+            lblResult.Visible = true;
+            lblStatus.Visible = true;
+            pbBodyImage.Visible = true;
+
+            lblResult.Text = $"您的 BMI 是：{bmi}";
+
+            if (bmi < 18.5)
+            {
+                lblStatus.Text = "體態分級：體重過輕";
+                lblStatus.ForeColor = lblResult.ForeColor = Color.SteelBlue;
+                pbBodyImage.Image = Properties.Resources.體重過輕;
+            }
+            else if (bmi < 24)
+            {
+                lblStatus.Text = "體態分級：健康體位";
+                lblStatus.ForeColor = lblResult.ForeColor = Color.ForestGreen;
+                pbBodyImage.Image = Properties.Resources.健康體位;
+            }
+            else if (bmi < 27)
+            {
+                lblStatus.Text = "體態分級：過重";
+                // 這裡改成了較深、不刺眼的顏色
+                lblStatus.ForeColor = lblResult.ForeColor = Color.DarkGoldenrod;
+                pbBodyImage.Image = Properties.Resources.過重;
+            }
+            else if (bmi < 30)
+            {
+                lblStatus.Text = "體態分級：輕度肥胖";
+                lblStatus.ForeColor = lblResult.ForeColor = Color.Orange;
+                pbBodyImage.Image = Properties.Resources.輕度肥胖;
+            }
+            else if (bmi < 35)
+            {
+                lblStatus.Text = "體態分級：中度肥胖";
+                lblStatus.ForeColor = lblResult.ForeColor = Color.OrangeRed;
+                pbBodyImage.Image = Properties.Resources.中度肥胖;
+            }
+            else // bmi >= 35
+            {
+                lblStatus.Text = "體態分級：重度肥胖";
+                lblStatus.ForeColor = lblResult.ForeColor = Color.Red;
+                pbBodyImage.Image = Properties.Resources.重度肥胖;
+            }
+        }
+
+
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPrompt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
